58efee35a83af34b7bda49ec5dda3c081a4789a2
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-05-24 17:34:07 +1000

    restructured test folder

--- test/maestrano/saml/response_test.rb
+++ test/maestrano/saml/response_test.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../../test_helper', __FILE__)
+require File.expand_path('../../../test_helper', __FILE__)
 
 class SamlTest < Test::Unit::TestCase
   include SamlTestHelper
