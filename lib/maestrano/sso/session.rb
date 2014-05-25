f70cc318938399d7ee0528571f4637576687293c
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-05-25 14:54:14 +1000

    added session check methods

--- lib/maestrano/sso/session.rb
+++ lib/maestrano/sso/session.rb
@@ -12,9 +12,52 @@ module Maestrano
         end
         
         if self.uid.nil? || self.session_token.nil? || self.recheck.nil?
-          warn "Maestrano session information missing. Disabling session check"
+          warn "Maestrano session information missing. User will have to relogin"
         end 
       end
+      
+      def remote_check_required?
+        if self.uid && self.session_token && self.recheck
+          return (self.recheck <= Time.now)
+        end
+        return true
+      end
+      
+      # Check remote maestrano session and update the
+      # recheck attribute if the session is still valid
+      # Return true if the session is still valid and
+      # false otherwise
+      def perform_remote_check
+        # Get remote session info
+        url = Maestrano::SSO.session_check_url(self.uid, self.session_token)
+        begin
+          response = RestClient.get(url)
+          response = JSON.parse(response)
+        rescue Exception => e
+          response = {}
+        end
+        
+        # Process response
+        if response['valid'] && response['recheck']
+          self.recheck = Time.iso8601(response['recheck'])
+          return true
+        end
+        
+        return false
+      end
+      
+      def valid?
+        if self.remote_check_required?
+          if perform_remote_check
+            self.session[:mno_session_recheck] = self.recheck.utc.iso8601
+            return true
+          else
+            return false
+          end
+        end
+        return true
+      end
+      
     end
   end
 end
--- test/maestrano/sso/session_test.rb
+++ test/maestrano/sso/session_test.rb
@@ -15,4 +15,90 @@ class SessionTest < Test::Unit::TestCase
     assert_equal sso_session.session_token, @session[:mno_session]
     assert_equal sso_session.recheck, Time.iso8601(@session[:mno_session_recheck])
   end
+  
+  context "remote_check_required?" do
+    setup do
+      @sso_session = Maestrano::SSO::Session.new(@session)
+    end
+  
+    should "should return true if uid is missing" do
+      @sso_session.uid = nil
+      assert @sso_session.remote_check_required?
+    end
+    
+    should "should return true if session_token is missing" do
+      @sso_session.session_token = nil
+      assert @sso_session.remote_check_required?
+    end
+    
+    should "should return true if recheck is missing" do
+      @sso_session.recheck = nil
+      assert @sso_session.remote_check_required?
+    end
+    
+    should "return true if now is after recheck" do
+      Timecop.freeze(@sso_session.recheck + 60) do
+        assert @sso_session.remote_check_required?
+      end
+    end
+    
+    should "return false if now is before recheck" do
+      Timecop.freeze(@sso_session.recheck - 60) do
+        assert !@sso_session.remote_check_required?
+      end
+    end
+  end
+  
+  context "perform_remote_check" do
+    setup do
+      @sso_session = Maestrano::SSO::Session.new(@session)
+    end
+  
+    should "update the session recheck and return true if valid" do
+      recheck = @sso_session.recheck + 600
+      RestClient.stubs(:get).returns({'valid' => true, 'recheck' => recheck.utc.iso8601 }.to_json)
+      assert @sso_session.perform_remote_check
+      assert_equal @sso_session.recheck, recheck
+    end
+    
+    should "leave the session recheck unchanged and return false if invalid" do
+      recheck = @sso_session.recheck
+      RestClient.stubs(:get).returns({'valid' => false, 'recheck' => (recheck + 600).utc.iso8601 }.to_json)
+      assert !@sso_session.perform_remote_check
+      assert_equal @sso_session.recheck, recheck
+    end
+  end
+  
+  context "valid?" do
+    setup do
+      @sso_session = Maestrano::SSO::Session.new(@session)
+    end
+  
+    should "return true if no remote_check_required?" do
+      @sso_session.stubs(:remote_check_required?).returns(false)
+      assert @sso_session.valid?
+    end
+    
+    should "return true if remote_check_required? and valid" do
+      @sso_session.stubs(:remote_check_required?).returns(true)
+      @sso_session.stubs(:perform_remote_check).returns(true)
+      assert @sso_session.valid?
+    end
+    
+    should "update session recheck timestamp if remote_check_required? and valid" do
+      recheck = (@sso_session.recheck + 600)
+      @sso_session.recheck = recheck
+      @sso_session.stubs(:remote_check_required?).returns(true)
+      @sso_session.stubs(:perform_remote_check).returns(true)
+      @sso_session.valid?
+      assert_equal @session[:mno_session_recheck], recheck.utc.iso8601
+    end
+    
+    should "return false if remote_check_required? and invalid" do
+      @sso_session.stubs(:remote_check_required?).returns(true)
+      @sso_session.stubs(:perform_remote_check).returns(false)
+      assert !@sso_session.valid?
+    end
+  end
+  
 end
