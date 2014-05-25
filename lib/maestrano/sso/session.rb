79fc531b486140f6f0e4ed498740234d3a6b0fec
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-05-25 13:54:48 +1000

    added initialization

--- lib/maestrano/sso/session.rb
+++ lib/maestrano/sso/session.rb
@@ -1,6 +1,16 @@
 module Maestrano
   module SSO
     class Session
+      attr_accessor :session, :uid, :session_token, :recheck
+      
+      def initialize(session)
+        self.session = session
+        self.uid = (self.session['mno_uid'] || self.session[:mno_uid])
+        self.session_token = (self.session['mno_session'] || self.session[:mno_session])
+        if recheck = (self.session['mno_session_recheck'] || self.session[:mno_session_recheck])
+          self.recheck = Time.iso8601(recheck)
+        end
+      end
     end
   end
 end
--- test/maestrano/sso/session_test.rb
+++ test/maestrano/sso/session_test.rb
@@ -1,4 +1,18 @@
 require File.expand_path('../../../test_helper', __FILE__)
 
 class SessionTest < Test::Unit::TestCase
+  setup do
+    @session = {
+      mno_uid: 'usr-1',
+      mno_session: 'g4dfg4fdg8378d6acf45',
+      mno_session_recheck: Time.now.utc.iso8601
+    }
+  end
+  
+  should "initialize the sso session properly" do
+    sso_session = Maestrano::SSO::Session.new(@session)
+    assert_equal sso_session.uid, @session[:mno_uid]
+    assert_equal sso_session.session_token, @session[:mno_session]
+    assert_equal sso_session.recheck, Time.iso8601(@session[:mno_session_recheck])
+  end
 end
