f57b886aaab10989b998cd5fb4e15abe144c803e
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-08-04 20:50:50 +1000

    added has_credit_card to Group

--- lib/maestrano/sso/base_group.rb
+++ lib/maestrano/sso/base_group.rb
@@ -2,13 +2,14 @@ module Maestrano
   module SSO
     class BaseGroup
       attr_accessor :local_id
-      attr_reader :uid,:country, :company_name, :free_trial_end_at
+      attr_reader :uid,:country, :company_name, :free_trial_end_at, :has_credit_card
       
       # Initializer
       # @param Maestrano::SAML::Response
       def initialize(saml_response)
         att = saml_response.attributes
         @uid = att['group_uid']
+        @has_credit_card = (att['group_has_credit_card'] == 'true')
         @country = att['country']
         @free_trial_end_at = Time.iso8601(att['group_end_free_trial'])
         @company_name = att['company_name']
@@ -21,6 +22,7 @@ module Maestrano
           info: {
             free_trial_end_at: self.free_trial_end_at,
             company_name: self.company_name,
+            has_credit_card: self.has_credit_card,
             country: self.country,
           },
           extra: {}
--- test/maestrano/sso/base_group_test.rb
+++ test/maestrano/sso/base_group_test.rb
@@ -8,19 +8,20 @@ module Maestrano
       setup do
         @saml_response = Maestrano::Saml::Response.new(response_document)
         @saml_response.stubs(:attributes).returns({
-          'mno_session'          => 'f54sd54fd64fs5df4s3d48gf2',
-          'mno_session_recheck'  => Time.now.utc.iso8601,
-          'group_uid'            => 'cld-1',
-          'group_end_free_trial' => Time.now.utc.iso8601,
-          'group_role'           => 'Admin',
-          'uid'                  => "usr-1",
-          'virtual_uid'          => "usr-1.cld-1",
-          'email'                => "j.doe@doecorp.com",
-          'virtual_email'        => "usr-1.cld-1@mail.maestrano.com",
-          'name'                 => "John",
-          "surname"              => "Doe",
-          "country"              => "AU",
-          "company_name"         => "DoeCorp"
+          'mno_session'           => 'f54sd54fd64fs5df4s3d48gf2',
+          'mno_session_recheck'   => Time.now.utc.iso8601,
+          'group_uid'             => 'cld-1',
+          'group_has_credit_card' => 'true',
+          'group_end_free_trial'  => Time.now.utc.iso8601,
+          'group_role'            => 'Admin',
+          'uid'                   => "usr-1",
+          'virtual_uid'           => "usr-1.cld-1",
+          'email'                 => "j.doe@doecorp.com",
+          'virtual_email'         => "usr-1.cld-1@mail.maestrano.com",
+          'name'                  => "John",
+          "surname"               => "Doe",
+          "country"               => "AU",
+          "company_name"          => "DoeCorp"
         })
       end
   
@@ -31,6 +32,7 @@ module Maestrano
       should "extract the rights attributes from the saml response" do
         group = Maestrano::SSO::BaseGroup.new(@saml_response)
         assert group.uid == @saml_response.attributes['group_uid']
+        assert group.has_credit_card == (@saml_response.attributes['group_has_credit_card'] == 'true')
         assert group.free_trial_end_at == Time.iso8601(@saml_response.attributes['group_end_free_trial'])
         assert group.company_name == @saml_response.attributes['company_name']
         assert group.country == @saml_response.attributes['country']
@@ -44,6 +46,7 @@ module Maestrano
           info: {
             free_trial_end_at: sso_group.free_trial_end_at,
             company_name: sso_group.company_name,
+            has_credit_card: sso_group.has_credit_card,
             country: sso_group.country,
           },
           extra: {}
